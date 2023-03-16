a938b29a7ea2d426224b29ba3292092435ae0a80
jigar <jigar.joshi011@gmail.com>
2023-03-16 11:25:24 +0530

    Worked on Registration API

--- Routes/register.js
+++ Routes/register.js
@@ -2,10 +2,11 @@ const express = require("express")
 const app = express();
 const router = express.Router()
 
-const {registerUser} = require('../controllers/userController')
+const {registerUser,getregisterUser} = require('../controllers/userController')
 
 // add routes
 router.route('/sign-up').post(registerUser)
+router.route('/sign-up').get(getregisterUser)
 // router.route('/sign-up').post(authUser)
 
 module.exports = app
