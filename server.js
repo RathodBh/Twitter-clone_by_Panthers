b96440805921780c137fe62d2bc1c56d28238f2f
thenameisvivek18 <vivekparekh1892@gmail.com>
2023-03-17 18:36:18 +0530

    Multer used

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const register = require('./Routes/register')
 const login = require('./Routes/login')
 const profile = require('./Routes/profile')
 const dashboard = require('./Routes/dashboard')
-
+const multer = require('multer')
 app.use(express.static(__dirname + '/public'))
 
 app.set('view engine','ejs')
@@ -20,11 +20,31 @@ app.use('/user-login',login)
 app.use('/dashboard',dashboard)
 app.use("/profile",profile)
 
-app.get("/logink",(req,res)=>{
-    res.render('login-b')
-})
+
 app.get("/login",(req,res)=>{
     res.render('login')
 })
+
+const upload = multer({
+    storage:multer.diskStorage({
+        destination:function(req,file,callback){
+            callback(null,"uploads")
+        },
+        filename:function(req,file,callback){
+            callback(null,file.fieldname+"-"+Date.now()+".jpg")
+        }
+    })
+}).single("uploaded_file");
+
+app.post("/upload",upload,(req,res)=>{
+    console.log(req.body,req.file);
+    res.send("file uploads")
+})
+
+app.get("/multer",(req,res)=>{
+    res.render('multer')
+})
+
+
 app.listen(PORT,()=>{ console.log(`I am listining on ${PORT}`);
 })
